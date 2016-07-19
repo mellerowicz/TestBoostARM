7440c8183cf99f245cdf7982c116fe005e6b583e
mellerowicz <bmellero@gmail.com>
2016-07-19 11:09:43 -0700

    First Commit of example code using Boost lib

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/bolek/workspace/Test_BoostARM/boost/lib/libboost_filesystem.a /home/bolek/workspace/Test_BoostARM/boost/lib/libboost_system.a /home/bolek/workspace/Test_BoostARM/boost/lib/libboost_regex.a /home/bolek/workspace/Test_BoostARM/boost/lib/libboost_thread.a
+
+LIBS := -lpthread -ldl
+
---
+++ Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+OBJS += \
+./main.o 
+
+CPP_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	arm-linux-gnueabihf-g++ -I"/home/bolek/workspace/Test_BoostARM/boost/include/boost" -I/usr/arm-linux-gnueabihf/include/c++/4.7.3 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
